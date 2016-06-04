85163734c745521136982fd45a2720de184b1571
soynog <nicolas.o.garcia@gmail.com>
2016-06-04 10:04:53 -0400

    Make Trip Fields Editable Spans
    - Gather contents into a data object on submit

--- app/components/trip-details/component.js
+++ app/components/trip-details/component.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  form: {},
+
+  actions: {
+    submit () {
+      let data = { trip: {} };
+      data.trip.name = this.$('span.trip-name').text();
+      data.trip.location = this.$('span.trip-location').text();
+      data.trip.description = this.$('span.trip-description').text();
+      console.log("Trip Update Form Submitted", data);
+    }
+  }
 });
