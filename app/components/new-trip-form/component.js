f9a965414fedb5c58078a36b7e25a85ed17031e9
soynog <nicolas.o.garcia@gmail.com>
2016-06-05 12:14:42 -0400

    Create Trip Action
    - Had to use component to get forms to work properly
    - Need to flesh out with all fields

--- app/components/new-trip-form/component.js
+++ app/components/new-trip-form/component.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  form: {},
+
+  actions: {
+    submit () {
+      console.log("Form is: ", this.form);
+      this.sendAction('submit', this.get('form'));
+    },
+  },
 });
--- app/trips/new/route.js
+++ app/trips/new/route.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  actions: {
+    createTrip (data) {
+      console.log("Create Trip Action Fired", data);
+      let trip = this.get('store').createRecord('trip', data);
+      return trip.save();
+    }
+  }
 });
