9ac5670a0035a8246a67ff44dd31c61c82a793fe
soynog <nicolas.o.garcia@gmail.com>
2016-06-07 09:48:20 -0400

    Request User Sign In on Trip Creation
    - Will not attempt to create trip if not signed in

--- app/trips/new/route.js
+++ app/trips/new/route.js
@@ -1,14 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  auth: Ember.inject.service(),
+  isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
+  flashMessages: Ember.inject.service(),
 
   actions: {
     createTrip (data) {
       console.log("Create Trip Action Fired", data);
-      Ember.set(data, 'start_date', new Date(data.start_date));
-      let trip = this.get('store').createRecord('trip', data);
-      console.log(trip);
-      return trip.save();
+      if(this.get('isAuthenticated')) {
+        Ember.set(data, 'start_date', new Date(data.start_date));
+        let trip = this.get('store').createRecord('trip', data);
+        console.log(trip);
+        return trip.save();
+      } else {
+        this.get('flashMessages').warning("Please sign in to create a trip!");
+      }
     }
   }
 });
