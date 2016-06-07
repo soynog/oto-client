12b55296713b0500a4a8ea93aaa84cdc3d270544
soynog <nicolas.o.garcia@gmail.com>
2016-06-07 11:49:14 -0400

    Create Invitation Model
    - HasMany on Trips still breaks
    - But, can display invitation data in trip details

--- app/trip/model.js
+++ app/trip/model.js
@@ -1,5 +1,6 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import { hasMany } from 'ember-data/relationships';
 // import { belongsTo } from 'ember-data/relationships';
 
 export default Model.extend({
@@ -8,5 +9,7 @@ export default Model.extend({
   startDate: attr('date'),
   endDate: attr('date'),
   location: attr('string'),
+  // invitations: hasMany('invitation'),
+  invitations: attr(),
   // user: belongsTo('user')
 });
