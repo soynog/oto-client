6e6757ed7d09ee1e238bf50f3a9e6c8b415e10d1
soynog <nicolas.o.garcia@gmail.com>
2016-06-07 10:03:03 -0400

    Transition to Trip Details Route on Create
    - Have to pass trip object as parameter to transitionTo

--- app/trips/new/route.js
+++ app/trips/new/route.js
@@ -10,9 +10,18 @@ export default Ember.Route.extend({
       console.log("Create Trip Action Fired", data);
       if(this.get('isAuthenticated')) {
         Ember.set(data, 'start_date', new Date(data.start_date));
+        // let trip = this.get('store').createRecord('trip', data);
+        // console.log(trip);
+        // return trip.save();
         let trip = this.get('store').createRecord('trip', data);
-        console.log(trip);
-        return trip.save();
+        trip.save()
+        .then((trip) => {
+          this.transitionTo('trip', trip);
+        })
+        .catch(() => {
+          this.get('flashMessages')
+          .danger('There was a problem. Please try again, making sure you have filled out all required fields.');
+        });
       } else {
         this.get('flashMessages').warning("Please sign in to create a trip!");
       }
