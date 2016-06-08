e0f3e3c29c1fc963c289208e2d294237550360db
soynog <nicolas.o.garcia@gmail.com>
2016-06-08 15:57:24 -0400

    User Initial Computed Property for participants
    - Invitations computed property tracks it from users

--- app/user/model.js
+++ app/user/model.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 import { hasMany } from 'ember-data/relationships';
 
@@ -7,4 +8,12 @@ export default DS.Model.extend({
   lastName: DS.attr('string'),
   // trips: hasMany('trip'),
   invitations: hasMany('invitation'),
+  initial: Ember.computed('firstName', function() {
+    let name = this.get('firstName');
+    if (name) {
+      return name.charAt(0);
+    } else {
+      return 'X';
+    }
+  }),
 });
