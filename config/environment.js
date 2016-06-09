73322b57cbfd8c71008bd3c77f4e3837e357027d
soynog <nicolas.o.garcia@gmail.com>
2016-06-09 19:19:49 -0400

    Update Base URL

--- config/environment.js
+++ config/environment.js
@@ -41,7 +41,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.baseURL = 'https://oto-api.herokuapp.com';
+    ENV.baseURL = '/';
     ENV.locationType = 'hash';
   }
 
