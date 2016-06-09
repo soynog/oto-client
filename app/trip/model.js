5b7b645c25ddaa53e59468bda86b99c39dbab017
soynog <nicolas.o.garcia@gmail.com>
2016-06-09 13:51:33 -0400

    Update Link generator to point to deployed app

--- app/trip/model.js
+++ app/trip/model.js
@@ -16,7 +16,7 @@ export default Model.extend({
     return Math.round((this.get('endDate') - this.get('startDate'))/(1000 * 60 * 60 * 24));
   }),
   url: Ember.computed('id', function() {
-    return `http://localhost:4200/trips/${this.get('id')}`;
+    return `http://soynog.github.io/oto-client/trips/${this.get('id')}`;
   }),
   // invitations: attr(),
   // users: hasMany('user')
