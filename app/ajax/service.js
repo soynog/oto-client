650121fc7922f14f82f2564a7827f9486d65e255
soynog <nicolas.o.garcia@gmail.com>
2016-06-09 19:16:39 -0400

    Update API host URL

--- app/ajax/service.js
+++ app/ajax/service.js
@@ -3,15 +3,15 @@ import AjaxService from 'ember-ajax/services/ajax';
 
 export default AjaxService.extend({
   auth: Ember.inject.service(),
+  host: 'https://oto-api.herokuapp.com',
   headers: Ember.computed('auth.credentials.token', {
-    get () {
+    get() {
       let headers = {};
       const token = this.get('auth.credentials.token');
       if (token) {
-        headers.Authorization = `Token token=${token}`;
+        headers['Authorization'] = `Token token=${token}`;
       }
-
       return headers;
-    },
-  }),
+    }
+  })
 });
